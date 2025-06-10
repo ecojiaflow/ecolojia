e4d46c2f9080fd674bcda943368572686b315216
Salim Benamara <salim.d.benamara@gmail.com>
2025-06-10 20:34:41 +0200

    Fix: Import Algolia ES6 pour PUT/DELETE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient, ConfidenceColor, VerifiedStatus } from '@prisma/client';
 import { execSync } from 'child_process';
-const algoliasearch = require('algoliasearch');
+import algoliasearch from 'algoliasearch';
 
 dotenv.config();
 
